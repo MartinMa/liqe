b94d81911c9df26c5ed0614e870ceade2bb1c14f
Martin Mädler <martin.maedler@gmail.com>
2023-02-14 15:39:45 +0100

    Fix wildcard handling

--- src/convertWildcardToRegex.ts
+++ src/convertWildcardToRegex.ts
@@ -4,7 +4,7 @@ export const convertWildcardToRegex = (pattern: string): RegExp => {
   return new RegExp(
     pattern
       .replace(WILDCARD_RULE, (_match, p1) => {
-        return p1 ? '(.+?)' : '(.)';
+        return p1 ? '(.*?)' : '(.)';
       }),
   );
 };
